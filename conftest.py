86debc537367966ac8bb508c2c82714fabf32ebd
Davit Aleksanyan <david-aleksanyan@mail.ru>
2023-03-09 03:07:21 +0400

    added conftest requirments allure_results

---
+++ conftest.py
@@ -0,0 +1,40 @@
+import pytest
+from selenium import webdriver
+from faker import Faker
+import random as rd
+
+
+@pytest.fixture(scope="function")
+def browser():
+    driver = webdriver.Firefox()
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture()
+def name():
+    faker = Faker('ru_RU')
+    first_name = faker.first_name()
+    return first_name
+
+
+@pytest.fixture()
+def last_name():
+    faker = Faker('ru_RU')
+    last_name = faker.last_name()
+    return last_name
+
+
+@pytest.fixture()
+def address():
+    faker = Faker('ru_RU')
+    address = faker.street_name()
+    return address
+
+
+@pytest.fixture()
+def phone():
+    country_code = '+7'
+    digits = rd.randint(1000000000,9999999999)
+    tel_number = country_code + str(digits)
+    return tel_number
